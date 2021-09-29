94451a40294dadb04a959b82bf42d4fa617f77e8
Michal Staněk <michal.stanek@avast.com>
2021-09-29 12:33:51 +0200

    Improve view responses

--- service_app/views.py
+++ service_app/views.py
@@ -1,12 +1,11 @@
 from django.http.response import JsonResponse, FileResponse
-from django.http import Http404
-from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework import generics
 from rest_framework import parsers
 
 from .models import Document, Page
-from .serializers import DocumentSerializer, PageSerializer
+from .serializers import DocumentSerializer
 from .tasks import render_images
 
 
@@ -16,30 +15,21 @@ class DocumentUploadView(generics.GenericAPIView):
 
     def post(self, request):
         serializer = DocumentSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            document = serializer.save()
-            render_images.send(document.pk)
-            return JsonResponse({"id": document.pk}, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        document = serializer.save()
+        render_images.send(document.pk)
+        return JsonResponse({"id": document.pk}, status=status.HTTP_201_CREATED)
 
 
 class DocumentView(generics.GenericAPIView):
 
     def get(self, request, pk):
-        try:
-            document = Document.objects.get(pk=pk)
-            return JsonResponse({"status": document.status, "n_pages": document.pages}, status=status.HTTP_200_OK)
-        except Document.DoesNotExist:
-            raise Http404("Document does not exist")
+        document = get_object_or_404(Document, pk=pk)
+        return JsonResponse({"status": document.status, "n_pages": document.pages}, status=status.HTTP_200_OK)
 
 
 class PageView(generics.GenericAPIView):
 
     def get(self, request, pk, num):
-        try:
-            page = Page.objects.get(page_num=num, document=pk).page_img
-            serializer = PageSerializer(page)
-            return FileResponse(page, content_type='image/png')
-        except Page.DoesNotExist:
-            raise Http404("Page does not exist")
+        page_img = get_object_or_404(Page, page_num=num, document=pk).page_img
+        return FileResponse(page_img, content_type='image/png')
